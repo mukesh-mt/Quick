c0e3bdc25dfe420e8e1f467b55681d9ca79a08e9
Mukesh Yadav <mukesh.yadav@mindtickle.com>
2022-11-08 18:56:34 +0530

    update package

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 let package = Package(
     name: "Quick",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
         .library(name: "Quick", targets: ["Quick"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", .exactItem("9.0.0")),
     ],
     targets: {
         var targets: [Target] = [
